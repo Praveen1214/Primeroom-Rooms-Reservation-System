1d85a420ccb5f19b12984f8aef16015773e26954
Dileepa Praveen <118685287+Praveen1214@users.noreply.github.com>
2023-12-19 20:05:54 +0530

    praveen

---
+++ routes/bookingRoute.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const Room = require("../models/rooms");
+
+router.get("/getallrooms", async (req, res) => {
+  try {
+    const rooms = await Room.find({});
+    return res.json({ rooms });
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
+});
+
+module.exports = router;
