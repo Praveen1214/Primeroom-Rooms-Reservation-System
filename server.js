d6163ce826c760a3e871df4f59592236db86e128
Dileepa Praveen <118685287+Praveen1214@users.noreply.github.com>
2024-01-22 01:42:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,5 +13,14 @@ app.use("/api/rooms", roomsRoute);
 app.use("/api/users", userRoute);
 app.use("/api/bookings", bookingRoute);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client", "build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+}
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server runnging on port ${port}`));
+
+app.listen(port, () =>
+    console.log(`Server running on port ${port} with nodemon`)
+);
