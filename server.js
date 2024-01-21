4738390d2cfe433aa45f2c59ba7edc5397a4c6a4
praveen1214 <it22564368@my.sliit.lk>
2024-01-22 02:58:56 +0530

    final

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.use("/api/users", userRoute);
 app.use("/api/bookings", bookingRoute);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client", "build")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    });
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
 const port = process.env.PORT || 5000;
 
 app.listen(port, () =>
-    console.log(`Server running on port ${port} with nodemon`)
+  console.log(`Server running on port ${port} with nodemon`)
 );
