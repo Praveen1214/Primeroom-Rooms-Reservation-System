da3d2209453b7f1eaeb3e92cac3475285d113cba
praveen1214 <it22564368@my.sliit.lk>
2024-01-23 00:26:59 +0530

    deploye

--- db.js
+++ db.js
@@ -1,17 +1,22 @@
-const mongoose = require("mongoose");
+const { default: mongoose } = require("mongoose");
+mongoose.set("strictQuery", true);
 
-var mongoURL = 'mongodb+srv://dileepapraveen32:eeDP1214.@praveen.jndze1l.mongodb.net/primerooms'
+var mongoURL =
+  "mongodb+srv://dileepapraveen32:eeDP1214.@praveen.jndze1l.mongodb.net/primerooms";
 
-mongoose.connect(process.env.MONGODB_URI || mongoURL ,{useUnifiedTopology : true ,useNewUrlParser : true ,})
-
-var connection = mongoose.connection
-
-connection.on('error',()=>{
-    console.log('Mongo DB connetion Fail')
+mongoose.connect(process.env.MONGODB_URI || mongoURL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 
-connection.on('connected',()=>{
-    console.log('Mongo DB connetion suc!')
+var connection = mongoose.connection;
+
+connection.on("error", () => {
+  console.log("MongDB Connection Failed");
+});
+
+connection.on("connected", () => {
+  console.log("MongoDB Connection Successful");
 });
 
 module.exports = mongoose;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
@@ -12,14 +13,23 @@ app.use(express.json());
 app.use("/api/rooms", roomsRoute);
 app.use("/api/users", userRoute);
 app.use("/api/bookings", bookingRoute);
+const port = process.env.PORT || 5000;
 
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client", "build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//   });
+// }
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  // Set the static folder
+  app.use(express.static("client/build"));
+
+  // Serve the index.html file for all non-API routes
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-const port = process.env.PORT || 5000;
 
 app.listen(port, () =>
   console.log(`Server running on port ${port} with nodemon`)
