da0c31052ec950bbc03084ee781d1b7def442ce5
praveen1214 <it22564368@my.sliit.lk>
2024-01-22 13:55:26 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,23 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-var mongoURL = 'mongodb+srv://dileepapraveen32:eeDP1214.@praveen.jndze1l.mongodb.net/primerooms'
+var mongoURL =
+  "mongodb+srv://dileepapraveen32:eeDP1214.@praveen.jndze1l.mongodb.net/primerooms";
 
-mongoose.connect(process.env.MONGODB_URI || mongoURL ,{useUnifiedTopology : true ,useNewUrlParser : true ,})
-
-var connection = mongoose.connection
-
-connection.on('error',()=>{
-    console.log('Mongo DB connetion Fail')
+mongoose.connect(process.env.MONGODB_URI || mongoURL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 
-connection.on('connected',()=>{
-    console.log('Mongo DB connetion suc!')
+var connection = mongoose.connection;
+
+connection.on("error", () => {
+  console.log("Mongo DB connetion Fail");
+});
+
+connection.on("connected", () => {
+  console.log("Mongo DB connetion suc!");
 });
 
 module.exports = mongoose;
